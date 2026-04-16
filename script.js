54074d9e53e669c3cd474a189d32929e630320a5
Elvis <cristopherfuaaa@gmail.com>
2026-04-15 19:19:25 -0500

    ajuste de clases dinamicas para nav-scrolled

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ document.addEventListener('DOMContentLoaded', () => {
     const nav = document.querySelector('.main-nav');
     window.addEventListener('scroll', () => {
         if (window.scrollY > 50) { nav.classList.add('nav-scrolled'); }
+        else { nav.classList.remove('nav-scrolled'); }
+    });
